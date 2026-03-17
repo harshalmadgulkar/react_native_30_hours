015b5ec77069c0acc43a938451b819bbc33c0e1a
Harshal <harshal.madgulkar725@gmail.com>
2026-03-17 07:21:39 +0530

    Implement stack navigation

--- todo-app-rn/app/_layout.tsx
+++ todo-app-rn/app/_layout.tsx
@@ -2,5 +2,10 @@ import { Stack } from "expo-router";
 import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+    </Stack>
+  );
 }
--- todo-app-rn/app/index.tsx
+++ todo-app-rn/app/index.tsx
@@ -1,16 +1,32 @@
+import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>Hi</Text>
+
+      <Link href={'/about'}>Visit about screen</Link>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'black',
+    gap: 10
+  },
+  content: {
+    fontSize: 18,
+    // color: 'green'
+  }
+});
